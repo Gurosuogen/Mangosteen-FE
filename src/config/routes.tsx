f80bc3687d266481f8f6de7891e5bc507db848b5
gurosuogen <zcw5321@gmail.com>
2022-11-03 15:13:36 +0800

    feature:创建欢迎界面的页面

---
+++ src/config/routes.tsx
@@ -0,0 +1,26 @@
+import { RouteRecordRaw } from "vue-router";
+import { Bar } from "../views/Bar";
+import { Foo } from "../views/Foo";
+import { Welcome } from "../views/Welcome"
+import { First } from "../components/Welcome/First";
+import { Second } from "../components/Welcome/Second";
+import { Third } from "../components/Welcome/Third";
+import { Fourth } from "../components/Welcome/Fourth";
+
+
+
+
+export const routes:RouteRecordRaw[] = [
+    { path: '/', component: Foo },
+    { path: '/about', component: Bar },
+    {
+        path: '/welcome',
+        component: Welcome , 
+        children: [
+            { path:'1',component:First },
+            { path:'2',component:Second },
+            { path:'3',component:Third },
+            { path:'4',component:Fourth }
+        ] 
+    }
+]
