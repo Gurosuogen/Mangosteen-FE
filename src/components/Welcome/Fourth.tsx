410fe1842bf4df64ca978402f29f2ca74abaa111
gurosuogen <zcw5321@gmail.com>
2022-11-07 00:01:40 +0800

    重构WelcomeLayout组件

--- src/components/Welcome/Fourth.tsx
+++ src/components/Welcome/Fourth.tsx
@@ -1,23 +1,19 @@
-import { defineComponent } from 'vue';
-import s from './First.module.scss'
+import s from './WelcomeLayout.module.scss'
 import cloud from '../../assets/icons/cloud.svg'
+import { WelcomeLayout } from './WelcomeLayout';
 import { RouterLink } from 'vue-router';
+import { FunctionalComponent } from 'vue';
 
 
-export const Fourth = defineComponent({
-    setup: (props, context) => {
-        return () => (
-            <div class={s.wrapper}>
-                <div class={s.card}>
-                    <img class={s.icon} src={cloud}/>
-                    <h2>会挣钱<br/>还要会省钱</h2>
-                </div>
-                <div class={s.actions}>
+export const Fourth:FunctionalComponent = () => {
+        return <WelcomeLayout>{{
+            icon: () => <img src={cloud} />,
+            title: () => <h2>每日提醒<br/>不遗漏每一笔账单</h2>,
+            buttons: () => <>
                 <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-                    <RouterLink  to="/welcome/2">下一页</RouterLink>
-                    <RouterLink to="/start">跳过</RouterLink>
-                </div>
-            </div>
-        )
+                <RouterLink  to="/start">下一页</RouterLink>
+                <RouterLink to="/start">跳过</RouterLink>
+            </>
+        }}</WelcomeLayout>
     }
-})
+    Fourth.displayName = 'Fourth'
--- src/components/Welcome/Second.tsx
+++ src/components/Welcome/Second.tsx
@@ -1,23 +1,19 @@
-import { defineComponent } from 'vue';
-import s from './First.module.scss'
+import s from './WelcomeLayout.module.scss'
 import clock from '../../assets/icons/clock.svg'
+import { WelcomeLayout } from './WelcomeLayout';
 import { RouterLink } from 'vue-router';
+import { FunctionalComponent } from 'vue';
 
 
-export const Second = defineComponent({
-    setup: (props, context) => {
-        return () => (
-            <div class={s.wrapper}>
-                <div class={s.card}>
-                    <img src={clock} class={s.icon}/>
-                    <h2>每日提醒<br/>不遗漏每一笔账单</h2>
-                </div>
-                <div class={s.actions}>
+export const Second:FunctionalComponent = () => {
+        return <WelcomeLayout>{{
+            icon: () => <img src={clock} />,
+            title: () => <h2>每日提醒<br/>不遗漏每一笔账单</h2>,
+            buttons: () => <>
                 <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-                    <RouterLink  to="/welcome/2">下一页</RouterLink>
-                    <RouterLink to="/start">跳过</RouterLink>
-                </div>
-            </div>
-        )
+                <RouterLink  to="/welcome/3">下一页</RouterLink>
+                <RouterLink to="/start">跳过</RouterLink>
+            </>
+        }}</WelcomeLayout>
     }
-})
+    Second.displayName = 'Second'
