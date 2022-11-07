81bfeaf7955700f66977dad1e35cff30484d32a5
gurosuogen <zcw5321@gmail.com>
2022-11-07 22:03:04 +0800

    完成欢迎界面动画

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,12 +1,13 @@
 import { RouteRecordRaw } from "vue-router"; 
-import { Welcome } from "../views/Welcome"
 import { First } from "../components/Welcome/First";
+import { FirstActions } from "../components/Welcome/FirstActions";
 import { Second } from "../components/Welcome/Second";
+import { SecondActions } from "../components/Welcome/SecondActions";
 import { Third } from "../components/Welcome/Third";
+import { ThirdActions } from "../components/Welcome/ThirdActions";
 import { Fourth } from "../components/Welcome/Fourth";
-
-
-
+import { FourthActions } from "../components/Welcome/FourthActions";
+import { Welcome } from "../views/Welcome"
 
 export const routes:RouteRecordRaw[] = [
     { path: '/', redirect:'/welcome' },
@@ -14,11 +15,11 @@ export const routes:RouteRecordRaw[] = [
         path: '/welcome',
         component: Welcome , 
         children: [
-            { path:'',redirect:'/welcome/1' },
-            { path:'1',component:First },
-            { path:'2',component:Second },
-            { path:'3',component:Third },
-            { path:'4',component:Fourth }
+            { path:'',redirect:'/welcome/1', },
+            { path:'1',components:{ main: First, footer: FirstActions }, },
+            { path:'2',components:{ main: Second, footer: SecondActions}, },
+            { path:'3',components:{ main: Third, footer: ThirdActions}, },
+            { path:'4',components:{ main: Fourth, footer: FourthActions}, },
         ] 
     }
 ]
