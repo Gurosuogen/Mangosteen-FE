98736e33139ae80b057be60c6eed95ad27ae31ad
gurosuogen <zcw5321@gmail.com>
2022-11-15 17:37:15 +0800

    完成数字输入

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -13,7 +13,33 @@ export const InputPad = defineComponent({
     setup: (props, context) => {
         const now = new Date()
         const refDate = ref<Date>(now)
-        const appendText = (n: number | string) => refAmount.value += n.toString()
+        const refAmount = ref('0')
+        const appendText = (n: number | string) => {
+            const nString = n.toString()
+            const dotIndex = refAmount.value.indexOf('.')
+            if (refAmount.value.length >= 13) {
+                return
+            }
+            if (dotIndex >=0 && refAmount.value.length - dotIndex > 2) {
+                return
+            }
+            if (nString === '.') {
+                if (dotIndex >= 0) {
+                    return
+                }
+            } else if (nString === '0') {
+                if (dotIndex === -1) {
+                    if (refAmount.value === '0') {
+                        return
+                    }
+                }
+            }else {
+                if (refAmount.value === '0') {
+                    refAmount.value = ''                    
+                }
+            }
+            refAmount.value += n.toString()
+        }
         const buttons = [
             {text: '1', onClick: () => { appendText(1) } },
             { text: '2', onClick: () => { appendText(2) } },
@@ -26,14 +52,13 @@ export const InputPad = defineComponent({
             { text: '9', onClick: () => { appendText(9) } },
             { text: '.', onClick: () => { appendText('.') } },
             { text: '0', onClick: () => { appendText(0) } },
-            { text: '清空', onClick: () => { } },
+            { text: '清空', onClick: () => { refAmount.value = '0' } },
             { text: '提交', onClick: () => { } },
         ]
         const refDatePickerVisible = ref(false)
         const showDatePicker =() => refDatePickerVisible.value = true
         const hideDatePicker =() => refDatePickerVisible.value = false
         const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
-        const refAmount = ref('')
         return () => <>
             <div class={s.dateAndAmount}>
                 <span class={s.date}>
