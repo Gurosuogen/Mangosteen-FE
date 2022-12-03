5f0b6af69c437d07282883da3b026afbf1360845
gurosuogen <zcw5321@gmail.com>
2022-12-03 15:33:51 +0800

    发送验证码成功之后触发倒计时 引入axios

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -46,7 +46,7 @@ export const FormItem = defineComponent({
         const timer = ref<number>()
         const count = ref<number>(props.countFrom)
         const isCounting = computed(() => !!timer.value)
-        const onClickSendValidationCode = () => {
+        const startCount = () => {
             props.onClick?.()
             timer.value = setInterval(() => {
                 count.value -= 1
@@ -57,6 +57,7 @@ export const FormItem = defineComponent({
                 }
             },1000)
         }
+        context.expose({ startCount })
         const content = computed(() => {
             switch (props.type) {
                 case 'text':
@@ -73,7 +74,7 @@ export const FormItem = defineComponent({
                     return <>
                         <input class={[s.formItem, s.input, s.validationCodeInput]}
                             placeholder={props.placeholder} />
-                        <Button disabled={isCounting.value} onClick={onClickSendValidationCode} class={[s.formItem, s.button, s.validationCodeButton]}>
+                        <Button disabled={isCounting.value} onClick={props.onClick} class={[s.formItem, s.button, s.validationCodeButton]}>
                             {isCounting.value ? `${count.value}秒后可重新发送` : '发送验证码'}
                         </Button>
                     </>
