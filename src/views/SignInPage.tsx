98eb5b095baeac7b2ebc839600cb315abc88eb6a
gurosuogen <zcw5321@gmail.com>
2022-12-14 22:27:19 +0800

    创建 mock 模块

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,17 +1,16 @@
 import axios, { AxiosResponse } from 'axios';
 import { defineComponent, PropType, reactive, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useBool } from '../hooks/useBool';
 import { MainLayout } from '../layouts/MainLayout';
 import { Button } from '../shared/Button';
 import { Form, FormItem } from '../shared/Form';
-import { Icon } from '../shared/Icon';
+import { history } from '../shared/history';
 import { http } from '../shared/Http';
+import { Icon } from '../shared/Icon';
+import { refreshMe } from '../shared/me';
 import { hasError, validate } from '../shared/validate';
 import s from './SignInPage.module.scss';
-import { useBool } from '../hooks/useBool';
-import { history } from '../shared/history';
-import { useRoute,useRouter } from 'vue-router';
-import { refreshMe } from '../shared/me';
-
 export const SignInPage = defineComponent({
     setup: (props, context) => {
         const formData = reactive({
@@ -20,14 +19,13 @@ export const SignInPage = defineComponent({
         })
         const errors = reactive({
             email: [],
-            code: ''
+            code: []
         })
         const refValidationCode = ref<any>()
         const { ref: refDisabled, toggle, on: disabled, off: enable } = useBool(false)
-        const route = useRoute()
         const router = useRouter()
+        const route = useRoute()
         const onSubmit = async (e: Event) => {
-            console.log('submit')
             e.preventDefault()
             Object.assign(errors, {
                 email: [], code: []
@@ -35,28 +33,32 @@ export const SignInPage = defineComponent({
             Object.assign(errors, validate(formData, [
                 { key: 'email', type: 'required', message: '必填' },
                 { key: 'email', type: 'pattern', regex: /.+@.+/, message: '必须是邮箱地址' },
-                { key: 'code', type: 'required', message: '必填' }
+                { key: 'code', type: 'required', message: '必填' },
             ]))
-            if(!hasError(errors)){
-                const response = await http.post<{ jwt: string }>('/session', formData)
+            if (!hasError(errors)) {
+                const response = await http.post<{ jwt: string }>('/session', formData, {
+                    params: { _mock: 'session' }
+                }).catch(onError)
+                console.log(response)
                 localStorage.setItem('jwt', response.data.jwt)
+                // router.push('/sign_in?return_to='+ encodeURIComponent(route.fullPath))
                 const returnTo = route.query.return_to?.toString()
                 refreshMe()
                 router.push(returnTo || '/')
             }
         }
         const onError = (error: any) => {
-            if(error.response.status === 422) {
-                Object.assign(errors, error.response.data.error)
+            if (error.response.status === 422) {
+                Object.assign(errors, error.response.data.errors)
             }
             throw error
         }
         const onClickSendValidationCode = async () => {
             disabled()
             const response = await http
-            .post('/validation_codes', { email: formData.email })
-            .catch(onError)
-            .finally(enable)
+                .post('/validation_codes', { email: formData.email })
+                .catch(onError)
+                .finally(enable)
             // 成功
             refValidationCode.value.startCount()
         }
@@ -68,7 +70,7 @@ export const SignInPage = defineComponent({
                     default: () => (
                         <div class={s.wrapper}>
                             <div class={s.logo}>
-                                <Icon name="mangosteen" class={s.icon}/>
+                                <Icon class={s.icon} name="mangosteen" />
                                 <h1 class={s.appName}>山竹记账</h1>
                             </div>
                             <Form onSubmit={onSubmit}>
@@ -79,15 +81,16 @@ export const SignInPage = defineComponent({
                                     placeholder='请输入六位数字'
                                     countFrom={1}
                                     disabled={refDisabled.value}
+                                    onClick={onClickSendValidationCode}
                                     v-model={formData.code} error={errors.code?.[0]} />
-                                <FormItem  style={{ paddingTop: '96px' }}>
+                                <FormItem style={{ paddingTop: '96px' }}>
                                     <Button type="submit">登录</Button>
                                 </FormItem>
                             </Form>
                         </div>
                     )
                 }
-                }</MainLayout>
+            }</MainLayout>
         )
     }
 })
