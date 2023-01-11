56223db475e7c399bc1996733d878e0735d70466
gurosuogen <zcw5321@gmail.com>
2023-01-11 15:06:12 +0800

    完成 bar chart

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -10,7 +10,7 @@ import { Time } from '../../shared/time';
 const DAY = 24 * 3600 * 1000
 type Data1Item = { happen_at: string, amount: number }
 type Data1 = Data1Item[]
-type Data2Item = { tag_id:number, tag: Tag, amount: number }
+type Data2Item = { tag_id: number, tag: Tag, amount: number }
 type Data2 = Data2Item[]
 export const Charts = defineComponent({
     props: {
@@ -25,35 +25,19 @@ export const Charts = defineComponent({
     },
     setup: (props, context) => {
         const kind = ref('expenses')
+
         const data1 = ref<Data1>([])
-        const betterData1 = computed<[string,number][]>(() => {
-            // if(!props.startDate || !props.endDate) {
-            //     return[]
-            // }
-            // const array = []
-            // const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
-            // const n = diff / DAY + 1
-            // let data1Index = 0
-            // for(let i =0; i<n; i++) {
-            //     const time = new Time(props.startDate + 'T00:00:00.000+0800').add(i,'day').getTimestamp()
-            //     if (data1.value[data1Index] && new Date(data1.value[data1Index].happen_at).getTime() === time) {
-            //         array.push([new Date(time).toISOString(), data1.value[data1Index].amount])
-            //         data1Index += 1
-            //     }else{
-            //         array.push([new Date(time).toISOString(),0])
-            //     }
-            // }
-            // return array as [string,number][]
+        const betterData1 = computed<[string, number][]>(() => {
             if (!props.startDate || !props.endDate) { return [] }
             const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
-            const n = diff / DAY +1
-            return Array.from({length: n}).map((_, i)=>{
-                const time = new Time(props.startDate+'T00:00:00.000+0800').add(i,'day').getTimestamp()
+            const n = diff / DAY + 1
+            return Array.from({ length: n }).map((_, i) => {
+                const time = new Time(props.startDate + 'T00:00:00.000+0800').add(i, 'day').getTimestamp()
                 const item = data1.value[0]
                 const amount = (item && new Date(item.happen_at).getTime() === time)
                     ? data1.value.shift()!.amount
                     : 0
-                return [new Date(time).toISOString(),amount]
+                return [new Date(time).toISOString(), amount]
             })
         })
         onMounted(async () => {
@@ -66,16 +50,25 @@ export const Charts = defineComponent({
             })
             data1.value = response.data.groups
         })
-        //data2
+
         const data2 = ref<Data2>([])
-        const betterData2 = computed<{name: string; value:number}[]>(()=>
+        const betterData2 = computed<{ name: string; value: number }[]>(() =>
             data2.value.map((item) => ({
                 name: item.tag.name,
                 value: item.amount
             }))
         )
-        onMounted(async ()=> {
-            const response = await http.get<{groups:Data2; summary: number}>('/items/summary',{
+
+        const betterData3 = computed<{ tag: Tag, amount: number, percent: number }[]>(() => {
+            const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
+            return data2.value.map(item => ({
+                ...item,
+                percent: Math.round(item.amount / total * 100)
+            }))
+        })
+
+        onMounted(async () => {
+            const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
                 happen_after: props.startDate,
                 happen_before: props.endDate,
                 kind: kind.value,
@@ -89,9 +82,9 @@ export const Charts = defineComponent({
                     { value: 'expenses', text: '支出' },
                     { value: 'income', text: '收入' }
                 ]} v-model={kind.value} />
-                <LineChart data={betterData1.value}/>
-                <PieChart data={betterData2.value}/>
-                <Bars />
+                <LineChart data={betterData1.value} />
+                <PieChart data={betterData2.value} />
+                <Bars data={betterData3.value} />
             </div>
         )
     }
