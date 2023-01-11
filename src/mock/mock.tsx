d6d15c7c7f4ff0272cc4d6e4579bc5124c0a63df
gurosuogen <zcw5321@gmail.com>
2023-01-11 15:12:35 +0800

    切换 kind 时重新请求数据

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -6,14 +6,39 @@ type Mock = (config: AxiosRequestConfig) => [number, any]
 faker.setLocale('zh_CN')
 
 export const mockItemSummary: Mock = (config) => {
-    if (config.params.group_by === 'happen_at') {
+    const { group_by, kind } = config.params
+    if (group_by === 'happen_at' && kind === 'expenses') {
         return [
             200,
             {
                 groups: [
-                    { happen_at: '2022-07-18T00:00:00.000+0800', amount: 100 },
-                    { happen_at: '2022-07-22T00:00:00.000+0800', amount: 300 },
-                    { happen_at: '2022-07-29T00:00:00.000+0800', amount: 200 }
+                    { happen_at: '2022-12-18T00:00:00.000+0800', amount: 100 },
+                    { happen_at: '2022-12-22T00:00:00.000+0800', amount: 300 },
+                    { happen_at: '2022-12-29T00:00:00.000+0800', amount: 200 }
+                ],
+                summary: 600
+            }
+        ]
+    } else if (group_by === 'happen_at' && kind === 'income') {
+        return [
+            200,
+            {
+                groups: [
+                    { happen_at: '2022-12-08T00:00:00.000+0800', amount: 100 },
+                    { happen_at: '2022-12-12T00:00:00.000+0800', amount: 300 },
+                    { happen_at: '2022-12-19T00:00:00.000+0800', amount: 200 }
+                ],
+                summary: 600
+            }
+        ]
+    } else if (group_by === 'tag_id' && kind === 'expenses') {
+        return [
+            200,
+            {
+                groups: [
+                    { tag_id: 1, tag: { id: 1, name: '交通', sign: faker.internet.emoji() }, amount: 100 },
+                    { tag_id: 2, tag: { id: 2, name: '吃饭', sign: faker.internet.emoji() }, amount: 300 },
+                    { tag_id: 3, tag: { id: 3, name: '购物', sign: faker.internet.emoji() }, amount: 200 }
                 ],
                 summary: 600
             }
@@ -23,11 +48,11 @@ export const mockItemSummary: Mock = (config) => {
             200,
             {
                 groups: [
-                    { tag_id: 1, tag: { id: 1, name: '交通',sign: faker.internet.emoji() }, amount: 100 },
-                    { tag_id: 2, tag: { id: 2, name: '吃饭',sign: faker.internet.emoji() }, amount: 300 },
-                    { tag_id: 3, tag: { id: 3, name: '购物',sign: faker.internet.emoji() }, amount: 200 }
+                    { tag_id: 1, tag: { id: 1, name: '交通', sign: faker.internet.emoji() }, amount: 400 },
+                    { tag_id: 2, tag: { id: 2, name: '吃饭', sign: faker.internet.emoji() }, amount: 300 },
+                    { tag_id: 3, tag: { id: 3, name: '购物', sign: faker.internet.emoji() }, amount: 200 }
                 ],
-                summary: 600
+                summary: 900
             }
         ]
     }
